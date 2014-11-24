32443e60d4c7453aee8a39fed60ca58ac1fd7ee2
Richard Lake <ralake1985@gmail.com>
2014-11-24 09:06:12 +0000

    Basic file structure

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'sinatra'
+gem 'bcrypt'
+gem 'rack-flash3'
+gem 'sinatra-partial'
+
+group :development, :test do
+  gem 'rspec'
+  gem 'database_cleaner'
+  gem 'capybara'
+  gem 'launchy'
+end
---
+++ app/server.rb
@@ -0,0 +1,15 @@
+require 'sinatra/base'
+require 'data_mapper'
+require 'rack-flash'
+require_relatve 'data_mapper_setup'
+
+class Chitter < Sinatra::Base
+
+  set :public_folder, Proc.new { File.join(root, "..", "public/css") }
+  use Rack::Flash
+  use Rack::MethodOverride
+  use Rack::Flash
+  use Rack::MethodOverride
+
+  run! if app_file == $0
+end
